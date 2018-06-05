51447b99d523d06aa3c95cf9167c58e56baa8892
carpon <carpon39038@163.com>
2018-06-05 21:20:50 +0800

    修改了切换书架上的书的方法 不再需要向服务器请求所有书目信息

--- src/Home.js
+++ src/Home.js
@@ -19,13 +19,39 @@ class Home extends React.Component {
         })
     }
     changeBookState = (book, shelf) => {
-        BooksAPI.update(book, shelf);
-        BooksAPI.getAll().then((books) => {
-            this.setState({ currentlyReadingBooks: books.filter((book) => (book.shelf === "currentlyReading")) });
-            this.setState({ wantToReadBooks: books.filter((book) => (book.shelf === "wantToRead")) });
-            this.setState({ ReadBooks: books.filter((book) => (book.shelf === "read")) });
-            // console.log(books);
-        })
+        const defaultShelf = book.shelf;
+        book.shelf = shelf;
+        BooksAPI.update(book, shelf).then(() => {
+            switch (defaultShelf) {
+                case "currentlyReading":
+                    this.setState({ currentlyReadingBooks: this.state.currentlyReadingBooks.filter((lastBook) => (lastBook.id !== book.id)) });
+                    break;
+                case "wantToRead":
+                    this.setState({ wantToReadBooks: this.state.wantToReadBooks.filter((lastBook) => (lastBook.id !== book.id)) });
+                    break;
+                case "read":
+                    this.setState({ ReadBooks: this.state.ReadBooks.filter((lastBook) => (lastBook.id !== book.id)) });
+                    break;
+                default:
+                    break;
+            }
+            switch (shelf) {
+                case "currentlyReading":
+                    this.state.currentlyReadingBooks.push(book);
+                    this.setState({ currentlyReadingBooks: this.state.currentlyReadingBooks });
+                    break;
+                case "wantToRead":
+                    this.state.wantToReadBooks.push(book);
+                    this.setState({ wantToReadBooks: this.state.wantToReadBooks });
+                    break;
+                case "read":
+                    this.state.ReadBooks.push(book);
+                    this.setState({ ReadBooks: this.state.ReadBooks });
+                    break;
+                default:
+                    break;
+            }
+        });
     }
     render() {
         return (
